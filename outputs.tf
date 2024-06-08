0c961f319553a42f4ba30bb01bef91293eab4352
Angel Castro <castrobasurto.angel@gmail.com>
2024-06-08 18:30:02 -0400

    added vm_id

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "vm_ip" {
     value = local.vm_ip
     description = "The IP address of the VM"
 }
+
+output "vm_id" {
+    value = local.vm_id
+    description = "The of the Virtual Machine recently created"
+}
